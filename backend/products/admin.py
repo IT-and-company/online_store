d7447bc84ada57bf788e9a96da6a263698d1b8b5
Мария Степанова <mstepanova188@gmail.com>
2023-07-27 19:29:37 +0300

    admin_type

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Category, Type)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'slug'
+    )
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'slug'
+    )
