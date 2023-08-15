e321096e2c4e16c4b0a85dc0d1e9bad5ce3c0a2c
Mashka33 <mstepanova188@gmail.com>
2023-08-15 12:58:17 +0300

    appdate

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
