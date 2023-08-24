5bed19e3529de3beaae010035eeb56e8c11f5e9e
Mashka33 <mstepanova188@gmail.com>
2023-08-24 10:07:52 +0300

    imports

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,9 @@
 # from django.db import models
 # from django.db.models import F, Sum
 # # from django.http import HttpResponse
-from django.db.models import Q, F, ExpressionWrapper, FloatField
-from django.shortcuts import get_object_or_404
+from django.db.models import F, Q
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
 from products.models import (Basket, Category, Favorite, Size, Tag, Type,
                              VariationProduct)
 from rest_framework import status, viewsets
